b9e7dcb3ca58d04887d46e5218ee62b3bbc88ad6
wen.li <wen.li@owitho.com>
2018-08-31 10:34:48 +0800

    add helloWorld

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: LiWen
+ * Date: 2018/8/30
+ * Time: 下午6:09
+ */
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("hello, welcome to my world!");
+
+    }
+}
